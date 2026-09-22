9f6914c99beb6cce00f13045bf721c7898ef7fc2
Suman Kumar <sumankumargin01234@gmail.com>
2026-09-23 04:38:21 +0530

    deploment error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import { getAuthHeaders } from "./auth";
 
-const API_BASE = "http://127.0.0.1:5000/api";
+const API_BASE = `${process.env.REACT_APP_API_URL || "http://127.0.0.1:5000"}/api`;
 
 export const scanWebsite = async (url) => {
   const res = await fetch(`${API_BASE}/scan/`, {
